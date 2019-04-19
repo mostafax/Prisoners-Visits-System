3542c992f86ac2164aef5dd09c6ab6bc56ad3bac
Mostafa Mohamed Mousa <mostafamousa1080p@gmail.com>
2019-04-19 15:19:43 +0200

    Added the intinal Forms

--- DB/DB/visitorSelection.cs
+++ DB/DB/visitorSelection.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Oracle.DataAccess.Client;
 using Oracle.DataAccess.Types;
+
 namespace DB
 {
     public partial class visitorSelection : Form
