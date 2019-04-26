5eb9c91ad2f4c820a1cc9da81f5aa7509a1d4bad
Mostafa Mohamed Mousa <mostafamousa1080p@gmail.com>
2019-04-26 09:25:54 +0200

    Adding all Forms

---
+++ DB-Form/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        CrystalReport1 cr;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cr = new CrystalReport1();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crystalReportViewer1.ReportSource = cr;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var form4 = (Form4)Tag;
+            form4.Show();
+            Close();
+        }
+    }
+}
---
+++ DB-Form/WindowsFormsApp1/WindowsFormsApp1/Form3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form3 : Form
+    {
+        CrystalReport3 cr;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            cr = new CrystalReport3();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crystalReportViewer1.ReportSource = cr;
+        }
+
+        private void crystalReportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var form4 = (Form4)Tag;
+            form4.Show();
+            Close();
+        }
+    }
+}
